9f1a36904107b5c4e2ece830d81da42af79117e5
Mr_limr267 <113236317+erha134@users.noreply.github.com>
2024-12-21 22:30:59 +0800

    entrypoint with modifier `final`

--- fabric/src/main/java/net/examplemod/fabric/client/ExampleModFabricClient.java
+++ fabric/src/main/java/net/examplemod/fabric/client/ExampleModFabricClient.java
@@ -3,7 +3,7 @@ package net.examplemod.fabric.client;
 import net.examplemod.client.ExampleModClient;
 import net.fabricmc.api.ClientModInitializer;
 
-public class ExampleModFabricClient implements ClientModInitializer {
+public final class ExampleModFabricClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         ExampleModClient.initClient();
--- forge/src/main/java/net/examplemod/forge/ExampleModForge.java
+++ forge/src/main/java/net/examplemod/forge/ExampleModForge.java
@@ -8,7 +8,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(ExampleMod.MOD_ID)
-public class ExampleModForge {
+public final class ExampleModForge {
     public ExampleModForge(Dist dist) {
         // Submit our event bus to let architectury register our content on the right time
         EventBuses.registerModEventBus(ExampleMod.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
