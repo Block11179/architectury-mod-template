721d375b3c04ce87f33565795d73fa5a0e04095d
Mr_limr267 <113236317+erha134@users.noreply.github.com>
2024-12-21 17:36:43 +0800

    fix mappings

--- common/src/main/java/net/examplemod/mixin/MixinTitleScreen.java
+++ common/src/main/java/net/examplemod/mixin/MixinTitleScreen.java
@@ -7,8 +7,10 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
-public class MixinTitleScreen {
-    @Inject(at = @At("HEAD"), method = "init()V")
+public abstract class MixinTitleScreen {
+    @Inject(method = "init()V",
+            at = @At("HEAD")
+    )
     private void init(CallbackInfo info) {
         System.out.println("Hello from example architectury common mixin!");
     }
