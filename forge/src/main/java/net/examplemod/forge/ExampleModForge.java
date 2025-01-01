18b67759dda01a8be1561912e95affee4957eec3
Mr_limr267 <113236317+erha134@users.noreply.github.com>
2025-01-01 15:54:21 +0800

    fix

--- forge/src/main/java/net/examplemod/forge/ExampleModForge.java
+++ forge/src/main/java/net/examplemod/forge/ExampleModForge.java
@@ -6,15 +6,16 @@ import net.examplemod.client.ExampleModClient;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.loading.FMLEnvironment;
 
 @Mod(ExampleMod.MOD_ID)
 public final class ExampleModForge {
-    public ExampleModForge(Dist dist) {
+    public ExampleModForge() {
         // Submit our event bus to let architectury register our content on the right time
         EventBuses.registerModEventBus(ExampleMod.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
         ExampleMod.init();
 
-        if (dist.isClient()) {
+        if (FMLEnvironment.dist.isClient()) {
             ExampleModClient.initClient();
         }
     }
