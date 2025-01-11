ef9ae84d407dfa101fdb59316bf68e7797492d33
Mr_limr267 <113236317+erha134@users.noreply.github.com>
2025-01-11 15:41:38 +0800

    up

--- fabric/src/main/java/net/examplemod/fabric/client/ExampleModClientFabric.java
+++ fabric/src/main/java/net/examplemod/fabric/client/ExampleModClientFabric.java
@@ -3,7 +3,7 @@ package net.examplemod.fabric.client;
 import net.examplemod.client.ExampleModClient;
 import net.fabricmc.api.ClientModInitializer;
 
-public final class ExampleModFabricClient implements ClientModInitializer {
+public final class ExampleModClientFabric implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         ExampleModClient.initClient();
